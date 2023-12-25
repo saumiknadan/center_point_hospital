1bedbf4b23bc9ccf52bbfa721b30440b2e1cbb0c
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2023-12-26 02:19:01 +0600

    appointment database

--- resources/views/user/appointment.blade.php
+++ resources/views/user/appointment.blade.php
@@ -14,13 +14,14 @@
             <input type="date" class="form-control">
           </div>
           <div class="col-12 col-sm-6 py-2 wow fadeInRight" data-wow-delay="300ms">
+            
             <select name="departement" id="departement" class="custom-select">
-              <option value="general">General Health</option>
-              <option value="cardiology">Cardiology</option>
-              <option value="dental">Dental</option>
-              <option value="neurology">Neurology</option>
-              <option value="orthopaedics">Orthopaedics</option>
-            </select>
+              <option value="general">--- Select Doctor ---</option>
+              @foreach ($doctors as $doctor )
+                <option value="{{ $doctor->name }}">Dr. {{ $doctor->name }} - {{ $doctor->speciality }}</option>
+              @endforeach
+          </select>
+
           </div>
           <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
             <input type="text" class="form-control" placeholder="Number..">
